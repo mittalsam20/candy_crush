3d0932c1e653f25152857ec857ec04daec6a3c47
Samaksh Mittal <samakshtoddle@Samakshs-MacBook-Pro.local>
2021-12-29 03:09:17 +0530

    randome board generator algo done

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,28 @@
-import './App.css';
+import { useEffect, useState } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+const App = () => {
+
+  const [currentBoard,setCurrentBoard]=useState([]);
+  const width = 8
+  const candyColors = ['blue', 'green', 'red', 'yellow', 'orange', 'yellow']
+
+  const createBoard = () => {
+    const myboard=[]
+    for (let i = 0; i < width * width; i++) {
+      const randomColor = candyColors[Math.floor(Math.random() * candyColors.length)];
+myboard.push(randomColor);
+    }
+    console.log(myboard)
+    setCurrentBoard(myboard);
+  }
+
+useEffect(()=>{
+  createBoard();
+},[])
+
+
+  return <div className="App"></div>
 }
 
-export default App;
+export default App
