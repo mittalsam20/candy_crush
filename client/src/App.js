d85790974e4f4f7b267269e59643d85be700df51
SAMAKSH MITTAL <sam.mittal44@gmail.com>
2021-12-29 21:25:57 +0530

    column of four added

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,68 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 const App = () => {
+  const [currentBoard, setCurrentBoard] = useState([]);
+  const candyColors = ["blue", "green", "red", "yellow", "orange", "yellow"];
+  const width = 8;
 
-  const [currentBoard,setCurrentBoard]=useState([]);
-  const width = 8
-  const candyColors = ['blue', 'green', 'red', 'yellow', 'orange', 'yellow']
+  const checkForColumnOfThree = () => {
+    for (let i = 0; i < 47; i++) {
+      const columnOfThree = [i, i + width, i + width * 2];
+      const decidedColor = currentBoard[i];
+
+      if (
+        columnOfThree.every((square) => currentBoard[square] === decidedColor)
+      )
+        columnOfThree.forEach((square) => (currentBoard[square] = " "));
+    }
+  };
+
+  const checkForColumnOfFour = () => {
+    for (let i = 0; i < 39; i++) {
+      const columnOfFour = [i, i + width, i + width * 2, i + width * 3];
+      const decidedColor = currentBoard[i];
+
+      if (columnOfFour.every((square) => currentBoard[square] === decidedColor))
+        columnOfFour.forEach((square) => (currentBoard[square] = " "));
+    }
+  };
 
   const createBoard = () => {
-    const myboard=[]
+    const myboard = [];
     for (let i = 0; i < width * width; i++) {
-      const randomColor = candyColors[Math.floor(Math.random() * candyColors.length)];
-myboard.push(randomColor);
+      const randomColor =
+        candyColors[Math.floor(Math.random() * candyColors.length)];
+      myboard.push(randomColor);
     }
     setCurrentBoard(myboard);
-    console.log(currentBoard)
-  }
+    console.log(currentBoard);
+  };
 
-useEffect(()=>{
-  createBoard();
-},[])
+  useEffect(() => {
+    createBoard();
+  }, []);
 
+  useEffect(() => {
+    const timer = setInterval(() => {
+      checkForColumnOfFour();
+      checkForColumnOfThree();
+      setCurrentBoard([...currentBoard]);
+    }, 100);
 
-  return <div className="app">
-    <div className="game">
-{currentBoard.map((ele,index)=>{
-console.log(ele);
-return(
-<img key={index} style={{backgroundColor:ele}}  />
+    return () => clearInterval(timer);
+  }, [checkForColumnOfFour, checkForColumnOfThree, currentBoard]);
 
-  )
-
-})}      
+  return (
+    <div className="app">
+      <div className="game">
+        {currentBoard.map((ele, index) => {
+          console.log(ele);
+          return <img key={index} style={{ backgroundColor: ele }} alt={ele} />;
+        })}
+      </div>
     </div>
-  </div>
-}
+  );
+};
 
-export default App
+export default App;
