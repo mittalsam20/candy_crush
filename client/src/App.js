6f917175b4e4b8b3014ed53bf4778fba3d3f71be
SAMAKSH MITTAL <sam.mittal44@gmail.com>
2021-12-31 14:59:42 +0530

    added highscore removed blank addition of score bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
-import blueCandy from "./images/blue-candy.png";
-import greenCandy from "./images/green-candy.png";
+import yellowCandy from "./images/yellow-candy.png";
 import orangeCandy from "./images/orange-candy.png";
 import purpleCandy from "./images/purple-candy.png";
+import greenCandy from "./images/green-candy.png";
+import blueCandy from "./images/blue-candy.png";
 import redCandy from "./images/red-candy.png";
-import yellowCandy from "./images/yellow-candy.png";
 import blank from "./images/blank.png";
 
+import ScoreBoard from "./components/scoreBoard";
 import "./App.css";
 
 const App = () => {
@@ -29,9 +30,12 @@ const App = () => {
     for (let i = 0; i <= 47; i++) {
       const columnOfThree = [i, i + width, i + width * 2];
       const decidedColor = currentBoard[i];
+      const isBlank = currentBoard[i] === blank;
 
       if (
-        columnOfThree.every((square) => currentBoard[square] === decidedColor)
+        columnOfThree.every(
+          (square) => currentBoard[square] === decidedColor && !isBlank
+        )
       ) {
         setScoreDisp((score) => score + 3);
         columnOfThree.forEach((square) => (currentBoard[square] = blank));
@@ -44,9 +48,12 @@ const App = () => {
     for (let i = 0; i <= 39; i++) {
       const columnOfFour = [i, i + width, i + width * 2, i + width * 3];
       const decidedColor = currentBoard[i];
+      const isBlank = currentBoard[i] === blank;
 
       if (
-        columnOfFour.every((square) => currentBoard[square] === decidedColor)
+        columnOfFour.every(
+          (square) => currentBoard[square] === decidedColor && !isBlank
+        )
       ) {
         setScoreDisp((score) => score + 4);
         columnOfFour.forEach((square) => (currentBoard[square] = blank));
@@ -62,10 +69,15 @@ const App = () => {
       const notValid = [
         6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64,
       ];
+      const isBlank = currentBoard[i] === blank;
 
       if (notValid.includes(i)) continue;
 
-      if (rowOfThree.every((square) => currentBoard[square] === decidedColor)) {
+      if (
+        rowOfThree.every(
+          (square) => currentBoard[square] === decidedColor && !isBlank
+        )
+      ) {
         setScoreDisp((score) => score + 3);
         rowOfThree.forEach((square) => (currentBoard[square] = blank));
         return true;
@@ -81,8 +93,14 @@ const App = () => {
         5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53,
         54, 55, 62, 63, 64,
       ];
+      const isBlank = currentBoard[i] === blank;
+
       if (notValid.includes(i)) continue;
-      if (rowOfFour.every((square) => currentBoard[square] === decidedColor)) {
+      if (
+        rowOfFour.every(
+          (square) => currentBoard[square] === decidedColor && !isBlank
+        )
+      ) {
         setScoreDisp((score) => score + 4);
         rowOfFour.forEach((square) => (currentBoard[square] = blank));
         return true;
@@ -224,6 +242,7 @@ const App = () => {
           );
         })}
       </div>
+      <ScoreBoard score={scoreDisp} />
     </div>
   );
 };
